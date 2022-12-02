7cc39cc16ddbb7b2095cfbe02c64b172b91628a6
Alex Coleman <alexjcoleman@gmail.com>
2022-12-02 08:48:38 +0000

    add quick clap parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    task: u8
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let args = Args::parse();
+
+    for _ in 0..args.task {
+        println!("This is an arg: {}", args.task)
+    }
 }
