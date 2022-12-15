3bdb70a355a288adaa42e2ffeb60738a748fef88
Sparrow0hawk <alexjcoleman@gmail.com>
2022-12-15 09:20:50 +0000

    added implementation for determining match outcome

--- src/two.rs
+++ src/two.rs
@@ -1,10 +1,31 @@
 use std::str::Split;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Choice {
     Rock,
     Paper,
     Scissors,
+}
+
+#[derive(Debug)]
+pub enum Outcome {
+    Win,
+    Lose,
+    Draw,
+}
+
+pub trait Winner {
+    fn winner(&self) -> Choice;
+}
+
+impl Winner for Choice {
+    fn winner(&self) -> Choice {
+        match self {
+            Self::Rock => Choice::Paper,
+            Self::Paper => Choice::Scissors,
+            Self::Scissors => Choice::Rock,
+        }
+    }
 }
 
 impl Choice {
@@ -24,7 +45,19 @@ pub struct Game {
     theirs: Choice,
 }
 
-pub fn match_hands(mut line: Split<&str>) -> Result<Game, &'static str> {
+impl Game {
+    fn find_winner(&self) -> Outcome {
+        let (ours_win, theirs_win) = (self.ours.winner(), self.theirs.winner());
+
+        match (ours_win, theirs_win) {
+            _ if ours_win == self.theirs => Outcome::Win,
+            _ if theirs_win == self.ours => Outcome::Lose,
+            _ => Outcome::Draw,
+        }
+    }
+}
+
+pub fn match_hands(mut line: Split<&str>) -> Result<Outcome, &'static str> {
     let p1 = match line.next() {
         Some(line) => line,
         None => return Err("No choice for player 1"),
@@ -40,5 +73,5 @@ pub fn match_hands(mut line: Split<&str>) -> Result<Game, &'static str> {
         theirs: Choice::convert(p1)?,
     };
 
-    Ok(round)
+    Ok(round.find_winner())
 }
