2c6f03606a77bada31e8297ec18d1115e48b488a
Sparrow0hawk <alexjcoleman@gmail.com>
2022-12-18 11:34:00 +0000

    update get_shared item logic

--- src/three.rs
+++ src/three.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 pub struct Backpack {
     items: String,
     length: Option<usize>,
-    shared_item: Option<HashMap<char, i32>>,
+    shared_item: Option<char>,
 }
 
 pub trait Length {
@@ -11,18 +11,26 @@ pub trait Length {
 }
 
 pub trait SharedItem {
-    fn get_shared(&self) -> HashMap<char, i32>;
+    fn get_shared(&self) -> char;
 }
 
 impl SharedItem for Backpack {
-    fn get_shared(&self) -> HashMap<char, i32> {
+    fn get_shared(&self) -> char {
+        // Find the duplicate letter in the pack
+        // not super happy with this function as it doesn't handle error cases at all
         let mut char_count: HashMap<char, i32> = HashMap::new();
 
         for ch in self.items.chars() {
             char_count.entry(ch).and_modify(|c| *c += 1).or_insert(1);
         }
 
-        char_count
+        let dup = char_count
+            .into_iter()
+            .max_by(|a, b| a.1.cmp(&b.1))
+            .map(|(k, _v)| k)
+            .unwrap();
+
+        dup
     }
 }
 
@@ -51,7 +59,7 @@ impl Backpack {
 pub fn solve_three(line: String) -> Result<Backpack, &'static str> {
     let pack = Backpack::new(line)?;
 
-    println!("{:?}", pack.items);
+    println!("{:?}", pack.shared_item);
     Ok(pack)
 }
 
@@ -65,7 +73,7 @@ mod tests {
 
         let pack = Backpack::new(line).unwrap();
 
-        assert_eq!(pack.items, String::from("aaBBcc"));
+        assert_eq!(pack.items, line);
         assert_eq!(pack.length, Some(6));
     }
 }
